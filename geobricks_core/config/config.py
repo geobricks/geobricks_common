3ab3852fde1db695de1e7bc80fdc215c04bd07df
vortex <simone.murzilli@fao.org>
2014-12-19 09:41:23 +0100

    added email utils

--- geobricks_core/config/config.py
+++ geobricks_core/config/config.py
@@ -1,21 +1,30 @@
 import logging
 
 config = {
+
     "settings": {
-        # To be used by Flask: DEVELOPMENT ONLY
-        "debug": True,
-
-        # Flask host: DEVELOPMENT ONLY
-        "host": "localhost",
-
-        # Flask port: DEVELOPMENT ONLY
-        "port": 5904,
-
         # Logging configurations
         "logging": {
             "level": logging.INFO,
             "format": "%(asctime)s | %(levelname)-8s | %(name)-20s | Line: %(lineno)-5d | %(message)s",
             "datefmt": "%d-%m-%Y | %H:%M:%s"
         },
-    }
+    },
+
+    # Folders
+    "folders": {
+        "tmp": "tmp_path",
+        "geoserver_datadir": "geoserver_data_folder",
+        "distribution": "distribution_folder",
+        "ftp": "ftp_folder",
+        # this is used by the filesystem to get the (published) layers in the file system
+        "workspace_layer_separator": "|"
+    },
+
+    # Email configurations (for now uses gmail as default client)
+    "email": {
+        "user":  "user",
+        "password": "password"
+    },
+
 }
