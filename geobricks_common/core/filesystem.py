dfbf69901aa9f0eec536976dde071e338dc1b3b5
simonefenix <simone.murzilli@fao.org>
2015-01-07 16:47:41 +0100

    added comment on code

--- geobricks_common/core/filesystem.py
+++ geobricks_common/core/filesystem.py
@@ -125,6 +125,7 @@ def get_raster_path(metadata):
 
     return None
 
+
 def get_raster_path_published_by_uid(uid, ext=".geotiff"):
     l = uid.split(workspace_layer_separator) if workspace_layer_separator in uid else uid.split(":")
     return os.path.join(config["settings"]["folders"]["geoserver_datadir"], "data",  l[0], l[1], l[1] + ext);
@@ -136,6 +137,7 @@ def get_raster_path_published(workspace, layername, ext=".geotiff"):
 
 def get_raster_path_storage(layername, ext=".geotiff"):
     return os.path.join(config["settings"]["folders"]["storage"], "raster",  layername, layername + ext);
+
 
 def get_raster_path_storage_by_uid(uid, ext=".geotiff"):
     return os.path.join(config["settings"]["folders"]["storage"], "raster", uid, uid + ext)
